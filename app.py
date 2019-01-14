0b6af5d3311b14416250d97aa1868dc8df7fe367
Lohith J <lohithj94@gmail.com>
2019-01-14 14:13:37 +0530

    assign port

--- app.py
+++ app.py
@@ -40,3 +40,12 @@ def home():
 
     return render_template(
         'index.html')
+
+
+
+
+
+        
+if __name__ == "__main__":
+    app.secret_key = '123456'
+    app.run(host="0.0.0.0", port=5000, debug=True)
